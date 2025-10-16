5cc92b97a6e25f9c8dc9bf577f6c6e306b3f379b
Balakrishnamd <balakrishna.md1234@gmail.com>
2025-10-16 14:22:57 +0530

    16 oct change

---
+++ project/src/Components/ProgressTracker.jsx
@@ -0,0 +1,7 @@
+export default function ProgressTracker() {
+   return (
+    <div>
+        <h1>This is our progress Tracker</h1>
+    </div>
+   )
+}
---
+++ project/src/Components/TaskList.jsx
@@ -0,0 +1,8 @@
+
+export default function TaskList() {
+    return (
+        <div>
+<h1> Task List complnent</h1>
+        </div>
+    )
+}
