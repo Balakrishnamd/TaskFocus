3316cec220b3bb1b98b15371fbd01c345a3ff40b
Balakrishnamd <balakrishna.md1234@gmail.com>
2025-10-18 10:47:07 +0530

    Completed

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,27 +1,51 @@
 
 import Taskform from "./Components/Taskform";
 import TaskList from "./Components/TaskList";
-import ProgressTracker from "./Components/ProgressTracker";
+import Progresstracker from "./Components/Progresstracker";
 import { useEffect, useState } from "react";
+import "./Style.css";
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
 
-useEffect(() => {
-  localStorage.setItem("tasks", JSON.stringify(tasks))
-});
+  useEffect(() => {
+    localStorage.setItem
+    ("tasks", JSON.stringify(tasks))
+  });
 
   const addTask = (task) => {
-setTasks([...tasks,task]);
+    setTasks([...tasks,task]);
   }
+
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+     newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+
+  const deleteTask = (index) => {
+      setTasks(tasks.filter((_, i) => i != index));
+  }
+
+  const clearTasks = () => {
+    setTasks([]);
+  }
+
   return(
-    <div>
-      <h1>Task Focus</h1>
-      <p> Friendly task Manager</p>
+    <div className="App">
+      <header>
+      <h1 className="title">Task Focus</h1>
+      <p className="tagline">Our friendly TaskManager</p>
+      </header>
       <Taskform addTask = {addTask}/>
-      <TaskList />
-      <ProgressTracker />
-      <button>Clear all task</button>
+      <TaskList tasks = {tasks} 
+      updateTask = {updateTask}
+      deleteTask = {deleteTask}/>
+      <Progresstracker tasks = {tasks}/>
+
+      {tasks.length>0 && 
+      (<button onClick={clearTasks} className="clear-btn">Clear all tasks</button>)}
+      
     </div>
   )
 }
--- project/src/Components/TaskList.jsx
+++ project/src/Components/TaskList.jsx
@@ -1,8 +1,27 @@
+export default function TaskList({tasks , updateTask, deleteTask}) {
 
-export default function TaskList() {
-    return (
-        <div>
-<h1> Task List complnent</h1>
-        </div>
-    )
+  const toggleComplete = (index)=>{
+        const updatedTask = {...tasks[index], completed: !tasks[index].completed};
+        updateTask(updatedTask, index);
+    }
+  return (
+    <ul className="task-list">
+      {tasks.map((task, index)=> (
+        <li key={index} className={task.completed ? "completed" : ""}>
+          <div>
+            <span>{task.text}
+              <small>({task.priority} , {task.category})</small>
+            </span>
+          </div>
+
+          <div>
+            <button onClick={() => toggleComplete(index)}>
+              {task.completed ? "Undo" : "Complete"}</button>
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </div>
+
+        </li>
+      ))}
+    </ul>
+  )
 }
